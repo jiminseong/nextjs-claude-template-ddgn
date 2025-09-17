d18c2b5eb187f7e5e0c45d2095e87e046c9455e5
jiminseong <iamjms4237@gachon.ac.kr>
2025-09-17 20:57:29 +0900

    api: move API initialization to a separate file and implement signup functionality in Home component

---
+++ src/app/api.tsx
@@ -0,0 +1,2 @@
+import { API } from "@deltalab-corp/ddgn-test";
+export const api = new API(process.env.NEXT_PUBLIC_API_URL!);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+import { api } from "@/app/api";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [nameState, setNameState] = useState<string>("test");
+  async function getInfo() {
+    const [res, error] = await api.account.signup({
+      name: nameState,
+    });
+    if (res) {
+      setNameState(String(res.id));
+    }
+    if (error == null) {
+    } else {
+      error.code;
+    }
+  }
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -20,17 +37,13 @@ export default function Home() {
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
+          <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <button
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            onClick={getInfo}
           >
             <Image
               className="dark:invert"
@@ -39,15 +52,15 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
-          </a>
+            TEST API DDGN
+          </button>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            {nameState}
           </a>
         </div>
       </main>
@@ -58,13 +71,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
           Learn
         </a>
         <a
@@ -73,13 +80,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
           Examples
         </a>
         <a
@@ -88,13 +89,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
           Go to nextjs.org →
         </a>
       </footer>
