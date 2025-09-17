5a4c3e112d7e7c776e3e3e0529a8d751e87212aa
jiminseong <iamjms4237@gachon.ac.kr>
2025-09-17 20:41:57 +0900

    api: update API URL to use environment variable for configuration

--- src/api/getInfo.tsx
+++ src/api/getInfo.tsx
@@ -1,5 +1,5 @@
 import { API } from "@deltalab-corp/ddgn-test";
-export const api = new API("https://api.deltalab.corp");
+export const api = new API(process.env.NEXT_PUBLIC_API_URL!);
 
 export async function getInfo() {
   const [res, error] = await api.account.signup({
