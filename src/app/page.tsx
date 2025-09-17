b3ef3d6c62a95f5c6dd93402319a175259703173
jiminseong <iamjms4237@gachon.ac.kr>
2025-09-17 21:44:11 +0900

    refactor: update project name and dependencies, remove unused API signup functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,6 @@
-"use client";
-import { api } from "@/app/api";
 import Image from "next/image";
-import { useState } from "react";
 
 export default function Home() {
-  const [nameState, setNameState] = useState<string>("test");
-  async function getInfo() {
-    const [res, error] = await api.account.signup({
-      name: nameState,
-    });
-    if (res) {
-      setNameState(String(res.id));
-    }
-    if (error == null) {
-    } else {
-      error.code;
-    }
-  }
-
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -37,13 +20,17 @@ export default function Home() {
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <button
+          <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            onClick={getInfo}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <Image
               className="dark:invert"
@@ -52,15 +39,15 @@ export default function Home() {
               width={20}
               height={20}
             />
-            TEST API DDGN
-          </button>
+            Deploy now
+          </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            {nameState}
+            Read our docs
           </a>
         </div>
       </main>
@@ -71,7 +58,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
           Learn
         </a>
         <a
@@ -80,7 +73,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
           Examples
         </a>
         <a
@@ -89,7 +88,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
           Go to nextjs.org →
         </a>
       </footer>
